96ea638a47f7daf19d1c16d6d11465421ab77652
sruthi-aswath <sruthiaswath06@gmail.com>
2025-08-29 23:37:05 +0530

    day1 learning

---
+++ day1.py
@@ -0,0 +1,43 @@
+thisdict = [{
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+},
+{
+  "brand": "sFord",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}]
+
+# for p in thisdict:
+#   print(p)
+#   print(p["brand"])
+ 
+listl = [1, 2, 3, 4, 5, 6]
+# for s in listl:
+#   print (s) 
+for s in range(0, len(listl) , 1):
+#   print(s)
+  print(listl[s])
+
+ 
+# thisdict = [{
+#   "brand": "Ford",
+#   "electric": False,
+#   "year": 1964,
+#   "colors": ["red", "white", "blue"]
+# },
+# {
+#   "brand": "sFord",
+#   "electric": False,
+#   "year": 1964,
+#   "colors": ["red", "white", "blue"]
+# }]
+# print(thisdict[0]["brand"])
+# print(thisdict[1]["brand"])
+
+# listl = [1, 2, 3]
+# for s in listl:
+#   print (s) 

1b99aa3e5408399ebd3018b393299c03a36bc3a6
sruthi-aswath <sruthiaswath06@gmail.com>
2025-08-30 11:55:09 +0530

    added method 2 for estimating max_marks and class average

---
+++ day2.py
@@ -0,0 +1,64 @@
+# student who score max marks
+grades = {"Amit": 85, "Riya": 92, "Karan": 78, "Neha": 88}
+
+# method1
+marks = []
+for student in grades:
+    print(grades[student])
+    marks.append(grades[student])
+print(marks)
+print(max(marks))
+
+marks_max = max(marks)
+for student in grades:
+    if (grades[student] == marks_max):
+        print(student)
+
+# method2:
+# find student with highest mark
+max_marks = max(grades.values())
+print(max_marks)
+
+for student in grades:
+    if (grades[student]) == max_marks:
+        print(student)
+
+# class average
+# method1:
+grades = {"Amit": 85, "Riya": 92, "Karan": 78, "Neha": 88}
+marks_list=[]
+for student in grades:
+    # print(student)
+    # print(grades[student])
+    marks = grades[student]
+    marks_list.append(marks)
+    
+print(marks_list)
+avg = sum(marks_list)/len(marks_list)
+print(avg)
+
+# method2:
+# claculate class average
+grades = {"Amit": 85, "Riya": 92, "Karan": 78, "Neha": 88}
+marks_sum = sum(grades.values())
+no_marks = len(grades)
+
+average = marks_sum / no_marks
+print(f"average is : {average}")
+
+
+# word frequency 
+
+sentence = "apple banana apple orange banana apple"
+sentence_split = sentence.split()
+print(sentence_split)
+wf = {}
+for word in sentence_split:
+    if word in wf:
+        wf[word] = wf[word] + 1
+    else:
+        wf[word] = 1
+        
+print(wf)        
+        
+    
